c23aa37dd4e8a2057056802a094b020a58d90004
Marcelo saad <marcelo_saad@hotmail.com>
2024-11-07 14:10:40 -0400

    telescope show hidden files and ignore .git files

--- nvim/.config/nvim/lua/saad/plugins/telescope.lua
+++ nvim/.config/nvim/lua/saad/plugins/telescope.lua
@@ -12,22 +12,37 @@ return {
 		local actions = require("telescope.actions")
 
 		telescope.setup({
-			-- pickers = {
-			-- 	find_files = { hidden = true },
-			-- },
-			defaults = {
-				path_display = { "smart" },
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+			pickers = {
+				find_files = {
+					file_ignore_patterns = { ".git", ".venv" },
+					hidden = true,
+				},
+				live_grep = {
+					file_ignore_patterns = { ".git", ".venv" },
+					additional_args = function(_)
+						return { "--hidden" }
+					end,
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+				defaults = {
+					path_display = { "smart" },
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous, -- move to prev result
+							["<C-j>"] = actions.move_selection_next, -- move to next result
+							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						},
 					},
 				},
 			},
 		})
 
 		telescope.load_extension("fzf")
+		-- telescope.load_extension("ui-select")
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
@@ -38,5 +53,8 @@ return {
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "[F]ind [K]eymaps" })
+		keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = "[F]ind [D]iagnostics" })
+		keymap.set("n", "<leader><leader>", "<cmd>Telescope buffers<cr>", { desc = "find existing buffers" })
 	end,
 }
