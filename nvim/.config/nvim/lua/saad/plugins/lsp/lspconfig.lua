4269dc24dd9a1516cc05da8728c2ae5ec8dbdee3
Marcelo saad <marcelo_saad@hotmail.com>
2024-11-11 18:31:33 -0400

    add yaml linter

--- nvim/.config/nvim/lua/saad/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/saad/plugins/lsp/lspconfig.lua
@@ -119,7 +119,7 @@ return {
 					capabilities = capabilities,
 					on_attach = function(client, bufnr)
 						vim.api.nvim_create_autocmd("BufWritePost", {
-							pattern = { "*.js", "*.ts" },
+							pattern = { "*.js", "*.ts", "*.yaml", "*.yml" },
 							callback = function(ctx)
 								-- Here use ctx.match instead of ctx.file
 								client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
