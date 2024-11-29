15a7e3f653312f54048a3cf78354c125acb9fee0
Marcelo saad <marcelo_saad@hotmail.com>
2024-11-28 20:21:09 -0400

    add noice plugin

--- nvim/.config/nvim/lua/saad/plugins/telescope.lua
+++ nvim/.config/nvim/lua/saad/plugins/telescope.lua
@@ -42,8 +42,7 @@ return {
 		})
 
 		telescope.load_extension("fzf")
-		-- telescope.load_extension("ui-select")
-
+		telescope.load_extension("noice")
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
