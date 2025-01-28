13bc97bda36a9b9440b904b90eb8cce728da0493
Marcelo saad <marcelo_saad@hotmail.com>
2025-01-28 10:34:10 -0400

    fix: new lspconfig

--- nvim/.config/nvim/lua/saad/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/saad/plugins/lsp/lspconfig.lua
@@ -4,40 +4,63 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/nvim-cmp",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
+		"antosha417/nvim-lsp-file-operations",
 		{ "folke/neodev.nvim", opts = {} },
-		dependencies = {
-			{
-				"rafamadriz/friendly-snippets",
-				config = function()
-					require("luasnip.loaders.from_vscode").lazy_load()
-				end,
-			},
+		{
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
 		},
 	},
 	config = function()
-		-- import lspconfig plugin
+		-- Importações iniciais
 		local lspconfig = require("lspconfig")
-
+		local mason_lspconfig = require("mason-lspconfig")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+		-- Habilidades de autocompletar para todos os LSPs
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Ensure Terraform files are recognized
+		-- Função on_attach para configurar mapeamentos locais
+		local on_attach = function(client, bufnr)
+			local opts = { noremap = true, silent = true, buffer = bufnr }
+
+			-- Mapeamentos básicos de LSP
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+			vim.keymap.set("n", "<leader>D", vim.diagnostic.open_float, opts)
+
+			-- Adicionando seus keybinds personalizados
+			vim.keymap.set("n", "<leader>ld", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help, opts)
+			vim.keymap.set("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder, opts)
+			vim.keymap.set("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder, opts)
+			vim.keymap.set("n", "<leader>lwl", function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, opts)
+		end
+
+		-- Diagnósticos personalizados
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
+		-- Adicionar suporte ao Terraform
 		vim.filetype.add({
 			extension = {
 				tf = "terraform",
 			},
 		})
 
-		-- Automatically start LSP for new Terraform files
-		vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-			pattern = "*.tf",
-			callback = function()
-				vim.bo.filetype = "terraform"
-			end,
-		})
-
-		-- ansible
+		-- LSPs específicos
 		lspconfig.ansiblels.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
@@ -45,17 +68,12 @@ return {
 			root_dir = lspconfig.util.root_pattern("roles", "playbooks"),
 		})
 
-		-- configuracao do terraform
 		lspconfig.terraformls.setup({
 			capabilities = capabilities,
 			filetypes = { "terraform", "tf" },
-			on_attach = function(_, bufnr)
-				local opts = { noremap = true, silent = true, buffer = bufnr }
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			end,
+			on_attach = on_attach,
 		})
 
-		-- Configuração para o LSP de YAML
 		lspconfig.yamlls.setup({
 			capabilities = capabilities,
 			filetypes = { "yaml", "yaml.kubernetes" },
@@ -69,138 +87,51 @@ return {
 						},
 						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
 						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-						["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+						["http://json.schemastore.org/ansible-playbook"] = "playbooks/**/*.{yml,yaml}",
+						["http://json.schemastore.org/ansible-stable-2.9"] = "roles/**/*.{yml,yaml}",
 						["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
 						["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
 						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
 						["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
-						["https://json.schemastore.org/ansible-playbook"] = "*/{roles,files}/*.{yml,yaml}",
 					},
 					validate = true,
 					completion = true,
 				},
 			},
 		})
-		-- import mason_lspconfig plugin
-		local mason_lspconfig = require("mason-lspconfig")
 
-		-- import cmp-nvim-lsp plugin
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-		local keymap = vim.keymap -- for conciseness
-
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local opts = { buffer = ev.buf, silent = true }
-
-				-- set keybinds
-				opts.desc = "Show LSP references"
-				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-				opts.desc = "Go to declaration"
-				keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-				opts.desc = "Show LSP implementations"
-				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-			end,
-		})
-
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
-
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-
+		-- Configurações para outros LSPs
 		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
+			-- Configuração padrão para todos os LSPs
 			function(server_name)
 				lspconfig[server_name].setup({
 					capabilities = capabilities,
+					on_attach = on_attach,
 				})
 			end,
+			-- Configuração para Svelte
 			["svelte"] = function()
-				-- configure svelte server
-				lspconfig["svelte"].setup({
+				lspconfig.svelte.setup({
 					capabilities = capabilities,
 					on_attach = function(client, bufnr)
+						on_attach(client, bufnr)
 						vim.api.nvim_create_autocmd("BufWritePost", {
 							pattern = { "*.js", "*.ts", "*.yaml", "*.yml" },
 							callback = function(ctx)
-								-- Here use ctx.match instead of ctx.file
-								client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+								if client.supports_method("$/onDidChangeTsOrJsFile") then
+									client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+								end
 							end,
 						})
 					end,
 				})
 			end,
-			["graphql"] = function()
-				-- configure graphql language server
-				lspconfig["graphql"].setup({
-					capabilities = capabilities,
-					filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-				})
-			end,
-			["emmet_ls"] = function()
-				-- configure emmet language server
-				lspconfig["emmet_ls"].setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
-					},
-				})
-			end,
+			-- Configuração para Lua
 			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
+				lspconfig.lua_ls.setup({
 					capabilities = capabilities,
 					settings = {
 						Lua = {
-							-- make the language server recognize "vim" global
 							diagnostics = {
 								globals = { "vim" },
 							},
