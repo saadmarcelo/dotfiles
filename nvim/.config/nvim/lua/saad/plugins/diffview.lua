ba2ee48b10b44a542174d14291131f98a60b9ba4
Marcelo Saad <marcelo.saad@sasi.com.br>
2025-04-23 16:22:07 -0400

    add: fix link

--- nvim/.config/nvim/lua/saad/plugins/diffview.lua
+++ nvim/.config/nvim/lua/saad/plugins/diffview.lua
@@ -1,6 +1,6 @@
 return {
 	"sindrets/diffview.nvim",
 	requires = "nvim-lua/plenary.nvim",
-	vim.keymap.set("n", "<leader>gdv", ":DiffviewOpen<CR>"),
-	vim.keymap.set("n", "<leader>gdf", ":DiffviewFileHistory<CR>"),
+	vim.keymap.set("n", "<leader>gpv", ":DiffviewOpen<CR>"),
+	vim.keymap.set("n", "<leader>gpf", ":DiffviewFileHistory<CR>"),
 }
