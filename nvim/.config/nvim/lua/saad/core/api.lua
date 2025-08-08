24e832ca6e2d2c8743f136a84415d77b4e58a11a
Marcelo Saad <marcelo.saad@sasi.com.br>
2025-08-08 10:08:20 -0400

    fix: update treesitter color terraform

--- nvim/.config/nvim/lua/saad/core/api.lua
+++ nvim/.config/nvim/lua/saad/core/api.lua
@@ -5,3 +5,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+vim.defer_fn(function()
+	vim.api.nvim_set_hl(0, "@property", { fg = "#000080" })
+	vim.api.nvim_set_hl(0, "@variable.member", { fg = "#9400D3" })
+end, 100)
